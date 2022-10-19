fb912f56a9712bf54da8189f8a89d67981aaa50e
Kristian Ceder <kceder@c2r3p10.hive.fi>
2022-10-19 12:32:17 +0300

    version updated

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -5,10 +5,10 @@ const Persons = ({persons}) => {
     persons.map(key => <li key={Math.random()}>{key.name} | number: {key.number}</li>)
   )
 }
-const Numbers = ({numbers}) => {
-  console.log(numbers);
+
+const Filter = ({value, onChange}) => {
   return (
-    numbers.map(key => <li>{key.numbers}</li>)
+    <p><input value={value} onChange={onChange}></input></p>
   )
 }
 const App = () => {
@@ -45,13 +45,10 @@ const App = () => {
     console.log(event.target.value);
     setNewNumber(event.target.value)
   }
-<<<<<<< HEAD
   const filterPersons = (event) => {
     const filter = event.target.value
     setNewFilter(filter)
   }
-=======
->>>>>>> 605c976001054aa088279b4e62a1579031ab7524
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterPerson, setNewFilter] = useState('')
@@ -61,17 +58,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter value={filterPerson} onChange={filterPersons}></Filter>
       <form onSubmit={addPerson}>
-<<<<<<< HEAD
         <div>name: <input onChange={handleChange}/></div>
         <div>number: <input onChange={handleNumChange}/></div>
-=======
-        <div>
-          name: <input onChange={handleChange}/>
-        </div>
-        <div>
-          number: <input onChange={handleNumChange}/>
-        </div>
->>>>>>> 605c976001054aa088279b4e62a1579031ab7524
         <div>
           <button disabled={newName.length > 0 ? false : true} type="submit">add</button>
         </div>
@@ -85,5 +73,7 @@ const App = () => {
         Debug num: {newNumber}
     </div>
   )
+
 }
+
 export default App
